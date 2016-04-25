b08400d53ef2dddb36ba5061e415d3774cc4816d
Andrew Fishberg <fishberg.github@gmail.com>
2016-04-24 18:12:24 -0700

    parser now works completely on complex sample program

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -2,6 +2,7 @@ package parser
 
 import scala.util.parsing.combinator._
 
+// Today I realized the difference with defining a function in () and {} when u want to use | as an Op
 
 object BSParser extends JavaTokenParsers with PackratParsers {
     //def apply(s: String): ParseResult[FuncDecl] = parseAll(pFuncDecl, s)
@@ -12,12 +13,12 @@ object BSParser extends JavaTokenParsers with PackratParsers {
             { case rType ~ rName ~ rParams ~ rBlock => FuncDecl(rType, rName, rParams, rBlock) }
 
     def pType: PackratParser[Type] = (
-        "int"       ^^^ IntTy()
-            |   "double"    ^^^ DoubleTy()
-            |   "bool"      ^^^ BoolTy()
-            |   "char"      ^^^ CharTy()
-            |   "void"      ^^^ VoidTy()
-        )
+            "int"       ^^^ IntTy()
+        |   "double"    ^^^ DoubleTy()
+        |   "bool"      ^^^ BoolTy()
+        |   "char"      ^^^ CharTy()
+        |   "void"      ^^^ VoidTy()
+    )
 
     def pName: PackratParser[String] = ident ^^ { rIdent => rIdent }
 
@@ -30,69 +31,121 @@ object BSParser extends JavaTokenParsers with PackratParsers {
     def pVarDecl: PackratParser[VarDecl] =
         pType ~ pName ^^ { case rType ~ rName => VarDecl(rType, rName) }
 
-    def pStatement: PackratParser[Statement] = (
-        pIfThenElse
-            |   pCall <~ ";"
-            |   pFor
-            |   pWhile
-            |   pBreak <~ ";"
-            |   pAssign <~ ";"
-            |   pDeclAssign <~ ";"
-            |   pVarDecl <~ ";"
-            |   pAddAssign <~ ";"
-            |   pSubAssign <~ ";"
-            |   pMultAssign <~ ";"
-            |   pDivAssign <~ ";"
-            |   pModAssign <~ ";"
-            |   pReturn <~ ";"
-        )
+    def pStatement: PackratParser[Statement] = pStatementBlock
 
-    def pReturn: PackratParser[Return] = {
-        "return" ~ pExpr ^^ { case "return" ~ rExpr => Return(rExpr) } | // TODO get | on next line?
-            "return" ^^^ Return(NullLit())
-    }
+    def pStatementBlock: PackratParser[Statement] = ( // TODO better name
+            pIfThenElse
+        |   pFor
+        |   pWhile
+        |   pStatementSemicolon
+    )
 
-    def pIfThenElse: PackratParser[IfThenElse] =
-        "if" ~ "(" ~ pExpr ~ ")" ~ pBlock ~ "else" ~ pBlock ^^
-            { case "if" ~ "(" ~ rExpr ~ ")" ~ rBlockTrue ~ "else" ~ rBlockFalse
-            => IfThenElse(rExpr, rBlockTrue, rBlockFalse) }
+    def pStatementSemicolon: PackratParser[Statement] = pStatementControlFlow <~ ";" // TODO better name
 
-    lazy val pExpr: PackratParser[Expr] = (
-        pExpr ~ "+" ~ pTerm ^^ { case rExpr ~ "+" ~ rTerm => AddOp(rExpr, rTerm) }
-            |   pExpr ~ "-" ~ pTerm ^^ { case rExpr ~ "-" ~ rTerm => SubOp(rExpr, rTerm) }
-            |   pTerm
-        )
+    def pStatementControlFlow: PackratParser[Statement] = ( // TODO better name
+            pCall
+        |   pBreak
+        |   pReturn
+        |   pStatementDecl
+    )
+
+    def pStatementDecl: PackratParser[Statement] = ( // TODO better name
+            pDeclAssign
+        |   pVarDecl
+        |   pStatementAssign
+    )
+
+    def pStatementAssign: PackratParser[Statement] = ( // TODO better name
+            pAssign
+        |   pAddAssign
+        |   pSubAssign
+        |   pMultAssign
+        |   pDivAssign
+        |   pModAssign
+        |   pInc            // TODO differentiation between ++i and i++
+        |   pDec
+    )
+
+    def pReturn: PackratParser[Return] = (
+            "return" ~ pExpr ^^ { case "return" ~ rExpr => Return(rExpr) }
+        |   "return" ^^^ Return(NullLit())
+    )
+
+    def pIfThenElse: PackratParser[IfThenElse] = "if" ~> pThenElse
+
+    def pThenElse: PackratParser[IfThenElse] = (
+            "(" ~ pExpr ~ ")" ~ pBlock ~ "else if" ~ pThenElse ^^
+                { case "(" ~ rExpr ~ ")" ~ rBlock ~ "else if" ~ rThenElse
+                => IfThenElse(rExpr, rBlock, Block(List(rThenElse)))}
+        |
+            "(" ~ pExpr ~ ")" ~ pBlock ~ "else" ~ pBlock ^^
+                { case "(" ~ rExpr ~ ")" ~ rBlockTrue ~ "else" ~ rBlockFalse
+                => IfThenElse(rExpr, rBlockTrue, rBlockFalse) }
+        |   "(" ~ pExpr ~ ")" ~ pBlock ^^
+            { case "(" ~ rExpr ~ ")" ~ rBlockTrue
+            => IfThenElse(rExpr, rBlockTrue, Block(List())) }
+    )
+
+    def pExpr: PackratParser[Expr] = pBoolOp
+
+    lazy val pBoolOp: PackratParser[Expr] = ( // TODO Order of operations for && and || etc
+            pBoolOp ~ ">"  ~ pBoolOp ^^ { case rBoolOp1 ~ ">"  ~ rBoolOp2 => GtOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ "<"  ~ pBoolOp ^^ { case rBoolOp1 ~ "<"  ~ rBoolOp2 => LtOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ ">=" ~ pBoolOp ^^ { case rBoolOp1 ~ ">=" ~ rBoolOp2 => GeqOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ "<=" ~ pBoolOp ^^ { case rBoolOp1 ~ "<=" ~ rBoolOp2 => LeqOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ "==" ~ pBoolOp ^^ { case rBoolOp1 ~ "==" ~ rBoolOp2 => EqOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ "!=" ~ pBoolOp ^^ { case rBoolOp1 ~ "!=" ~ rBoolOp2 => NeqOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ "||" ~ pBoolOp ^^ { case rBoolOp1 ~ "||" ~ rBoolOp2 => NeqOp(rBoolOp1, rBoolOp2) }
+        |   pBoolOp ~ "&&" ~ pBoolOp ^^ { case rBoolOp1 ~ "&&" ~ rBoolOp2 => NeqOp(rBoolOp1, rBoolOp2) }
+        |   pPlusMinus
+    )
+
+    lazy val pPlusMinus: PackratParser[Expr] = ( // TODO Why is | not happy on the next line?
+            pPlusMinus ~ "+" ~ pTerm ^^ { case rExpr ~ "+" ~ rTerm => AddOp(rExpr, rTerm) }
+        |   pPlusMinus ~ "-" ~ pTerm ^^ { case rExpr ~ "-" ~ rTerm => SubOp(rExpr, rTerm) }
+        |   pTerm
+    )
 
     lazy val pTerm: PackratParser[Expr] = (
-        pTerm ~ "*" ~ pFact ^^ { case rTerm ~ "*" ~ rFact => MultOp(rTerm, rFact) }
-            |   pTerm ~ "/" ~ pFact ^^ { case rTerm ~ "/" ~ rFact => DivOp(rTerm, rFact) }
-            |   pFact
-        )
+            pTerm ~ "*" ~ pFact ^^ { case rTerm ~ "*" ~ rFact => MultOp(rTerm, rFact) }
+        |   pTerm ~ "/" ~ pFact ^^ { case rTerm ~ "/" ~ rFact => DivOp(rTerm, rFact) }
+        |   pBinOp
+    )
+
+    def pBinOp: PackratParser[Expr] = (
+            "-" ~ pFact ^^ { case "-" ~ rFact => NegateOp(rFact) } // prevents from parsing -15 as NegateOp(IntLit(15))
+        |   "!" ~ pLit  ^^ { case "!" ~ rFact => NotOp(rFact) }
+        |   pLit
+    )
+
+    def pLit: PackratParser[Expr] = ( // TODO better name
+            pLiteral
+        |   pFact
+    )
 
     def pFact: PackratParser[Expr] = (
-        pEval
-            |   pVar
-            |   pLiteral
-            |   "("~>pExpr<~")"
-        )
+            pEval
+        |   pVar
+        |   "("~>pExpr<~")"
+    )
 
     def pLiteral: PackratParser[Literal] = (
-        pIntLit
-            |   pDoubleLit
-            |   pBoolLit
+            pIntLit
+        |   pDoubleLit
+        |   pBoolLit
         //|   pCharLit
-        )
+    )
 
     def pIntLit: PackratParser[IntLit] = wholeNumber ^^ { s => IntLit(s.toInt) }
 
     def pDoubleLit: PackratParser[DoubleLit] = decimalNumber ^^ { s => DoubleLit(s.toDouble) }
 
     def pBoolLit: PackratParser[BoolLit] = (
-        "true" ^^^ BoolLit(true)
-            |   "false" ^^^ BoolLit(false)
-        )
+            "true"  ^^^ BoolLit(true)
+        |   "false" ^^^ BoolLit(false)
+    )
 
-    //def pCharLit: PackratParser[CharLit] = "['].[']".r ^^ { case rChar => CharLit(rChar.charAt(1)) }
+    // def pCharLit: PackratParser[CharLit] = "'" ~ ".".r ~ "'" { case "'" ~ rChar ~ "'" => CharLit(rChar) }
 
     def pVar: PackratParser[Var] = pName ^^ { case rName => Var(rName) }
 
@@ -103,8 +156,8 @@ object BSParser extends JavaTokenParsers with PackratParsers {
     def pArgs: PackratParser[List[Expr]] = repsep(pExpr, ",")
 
     def pFor: PackratParser[For] =
-        "for" ~ "(" ~ pStatement ~ ";" ~ pExpr ~ ";" ~ pStatement ~ ")" ~ "{" ~ pBlock ~ "}" ^^
-            { case "for" ~ "(" ~ rStatement1 ~ ";" ~ rExpr ~ ";" ~ rStatement2 ~ ")" ~ "{" ~ rBlock ~ "}" => For(rStatement1, rExpr, rStatement2, rBlock) }
+        "for" ~ "(" ~ pStatementDecl ~ ";" ~ pExpr ~ ";" ~ pStatementAssign ~ ")" ~ pBlock ^^
+            { case "for" ~ "(" ~ rStatementDecl ~ ";" ~ rExpr ~ ";" ~ rStatementAssign ~ ")" ~ rBlock => For(rStatementDecl, rExpr, rStatementAssign, rBlock) }
 
     def pWhile: PackratParser[While] =
         "while" ~ "(" ~ pExpr ~ ")" ~ "{" ~ pBlock ~ "}" ^^
@@ -120,7 +173,7 @@ object BSParser extends JavaTokenParsers with PackratParsers {
         pVarDecl ~ "=" ~ pExpr ^^ { case rVarDecl ~ "=" ~ rExpr => DeclAssign(rVarDecl, rExpr) }
 
     def pAddAssign: PackratParser[AddAssign] =
-        pName ~ "+=" ~ pExpr ^^ { case rName ~ "=" ~ rExpr => AddAssign(rName, rExpr) }
+        pName ~ "+=" ~ pExpr ^^ { case rName ~ "+=" ~ rExpr => AddAssign(rName, rExpr) }
 
     def pSubAssign: PackratParser[SubAssign] =
         pName ~ "-=" ~ pExpr ^^ { case rName ~ "-=" ~ rExpr => SubAssign(rName, rExpr) }
@@ -129,16 +182,21 @@ object BSParser extends JavaTokenParsers with PackratParsers {
         pName ~ "*=" ~ pExpr ^^ { case rName ~ "*=" ~ rExpr => MultAssign(rName, rExpr) }
 
     def pDivAssign: PackratParser[DivAssign] =
-        pName ~ "/=" ~ pExpr ^^ { case rName ~ "=" ~ rExpr => DivAssign(rName, rExpr) }
+        pName ~ "/=" ~ pExpr ^^ { case rName ~ "/=" ~ rExpr => DivAssign(rName, rExpr) }
 
     def pModAssign: PackratParser[ModAssign] =
-        pName ~ "%=" ~ pExpr ^^ { case rName ~ "=" ~ rExpr => ModAssign(rName, rExpr) }
+        pName ~ "%=" ~ pExpr ^^ { case rName ~ "%=" ~ rExpr => ModAssign(rName, rExpr) }
 
+    def pInc: PackratParser[PostIncOp] =
+        pName ~ "++" ^^ { case rName ~ "++" => PostIncOp(rName) }
 
-    //    lazy val pStatement: PackratParser[Statement] =
-    //        (
-    //            rep1sep(pStatement, ";") ^^ Block
-    //        |   pIfThenElse )
-    //
-    //    def
+    def pDec: PackratParser[PostIncOp] =
+        pName ~ "--" ^^ { case rName ~ "++" => PostIncOp(rName) }
+
+//    lazy val pStatement: PackratParser[Statement] =
+//        (
+//            rep1sep(pStatement, ";") ^^ Block
+//        |   pIfThenElse )
+//
+//    def
 }
